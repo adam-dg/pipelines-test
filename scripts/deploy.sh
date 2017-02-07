9808f332bf754e87729a8c56213ab80c680a5a4e
Adam Dowling <adamd@deeson.co.uk>
2017-02-07 15:49:18 +0000

    - Updated deployment script with logic to setup SSH keys for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,21 @@
 #/usr/bin/env bash
 
-echo 'Hello World'
+KEYFILE=~/.ssh/deploy_id_rsa
+
+# Set up SSH access to the deployment target.
+mkdir -p ~/.ssh;
+
+echo ${DEPLOY_PRIVATE_KEY} | base64 --decode > ${KEYFILE};
+
+cat << EOF >> ~/.ssh/config
+Host ${DEPLOY_HOST}
+    UserKnownHostsFile=/dev/null
+    StrictHostKeyChecking=no
+    IdentityFile=${KEYFILE}
+EOF
+chmod 700 ~/.ssh;
+chmod 600 ~/.ssh/config;
+
+mkdir -p /tmp/checkout-test && cd /tmp/checkout-test && git clone ${DEPLOY_URL} .
+
+ls -lA /tmp/checkout-test
