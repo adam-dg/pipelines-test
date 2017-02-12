9a060d8d16da97ad3411014d6ed6d710a9215889
Adam Dowling <adamd@deeson.co.uk>
2017-02-12 23:04:23 +0000

    - Testing Git Relay Git email configuration via command line option

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -62,6 +62,6 @@ ls -lA /tmp/checkout-test
 # Relay commit to deployment repo
 #
 
-/opt/ci-tools/git-relay/git-relay-push.sh --src-repo-path=/opt/atlassian/bitbucketci/agent/build --git-username="Jenkins Deeson"
+/opt/ci-tools/git-relay/git-relay-push.sh --src-repo-path=/opt/atlassian/bitbucketci/agent/build --git-username="Jenkins Deeson" --git-email="jenkins@deeson.co.uk"
 
 echo 'Relay complete'
