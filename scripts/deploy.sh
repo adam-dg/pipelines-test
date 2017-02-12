b905a712a584fae3fc6f2ac49c45dbfb5a5a1686
Adam Dowling <adamd@deeson.co.uk>
2017-02-12 20:41:54 +0000

    - Checkout CI tools

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,29 +2,63 @@
 
 set -e
 
+echo $UID
+echo $USER
+
 #
 # This block could be wrapped up into its on helper function or script : Begin
 #
 
-KEYFILE=~/.ssh/deploy_id_rsa
+BITBUCKET_KEYFILE=~/.ssh/bitbucket_id_rsa
+
+DEPLOY_KEYFILE=~/.ssh/deploy_id_rsa
 
 # Set up SSH access to the deployment target.
 mkdir -p ~/.ssh
 
-echo ${DEPLOY_PRIVATE_KEY} | base64 --decode > ${KEYFILE}
-chmod 600 ${KEYFILE}
+echo ${BITBUCKET_PRIVATE_KEY} | base64 --decode > ${BITBUCKET_KEYFILE}
+chmod 600 ${BITBUCKET_KEYFILE}
+cat << EOF >> ~/.ssh/config
+
+Host bitbucket.org
+    UserKnownHostsFile=/dev/null
+    StrictHostKeyChecking=no
+    IdentityFile=${BITBUCKET_KEYFILE}
+
+EOF
+
+echo ${DEPLOY_PRIVATE_KEY} | base64 --decode > ${DEPLOY_KEYFILE}
+chmod 600 ${DEPLOY_KEYFILE}
 
 cat << EOF >> ~/.ssh/config
+
 Host ${DEPLOY_HOST}
     UserKnownHostsFile=/dev/null
     StrictHostKeyChecking=no
-    IdentityFile=${KEYFILE}
+    IdentityFile=${DEPLOY_KEYFILE}
+
 EOF
+
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/config
 
+
 #
 # This block could be wrapped up into its on helper function or script : End
+#
+
+
+#
+# Checkout CI tools
+#
+
+mkdir -p /opt/ci-tools
+cd /opt/ci-tools && git clone git@bitbucket.org:deesongroup6346/git-relay.git .
+ls -lA /opt/ci-tools
+ls -lA /opt/ci-tools/git-relay
+
+#
+# Testing our deployment keys work
 #
 
 mkdir -p /tmp/checkout-test && cd /tmp/checkout-test && git clone ${DEPLOY_URL} .
